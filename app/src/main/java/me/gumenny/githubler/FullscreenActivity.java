f7c9efb691e4b9352c6133494e16224d042176e8
Arkadius <humennyi.arkadii@gmail.com>
2017-08-28 23:39:36 +0300

    add splash screen

---
+++ app/src/main/java/me/gumenny/githubler/FullscreenActivity.java
@@ -0,0 +1,17 @@
+package me.gumenny.githubler;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class FullscreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
